a5512ec4ee9e2122c0d897ec470ba60dcd3bad44
Mattia Colombo <mattia.clmb@gmail.com>
2024-06-17 14:53:51 +0100

    Populating repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# Quiet Cove Quests
+
+
+ugh
---
+++ Source/QuietCoveQuests/QuietCoveQuestsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QuietCoveQuestsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AQuietCoveQuestsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AQuietCoveQuestsGameMode();
+};
+
+
+
